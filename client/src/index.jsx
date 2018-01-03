b33d5aaed9d6ba34111d3468e30b79f15b19666e
Albert Chang <albertchanged@gmail.com>
2018-01-03 10:54:49 -0800

    Re-add default search params in index.jsx, comment out dropDatabase()

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -42,6 +42,7 @@ class App extends React.Component {
       url: '/repos',
       method: 'GET',
       contentType: 'application/json',
+      data: {'username': 'albertchanged'},
       success: (data) => {
         console.log('GET success: ', data);
         this.setState({
