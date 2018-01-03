5e35d24daf1180c13611c5265b5bc885a92d02d3
Albert Chang <albertchanged@gmail.com>
2018-01-03 12:20:28 -0800

    Edit Repo.jsx to show Last Updated date rather than Created At

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -3,7 +3,7 @@ const Repo = (props) => (
   <div>
     <p><strong>Name: <a href={props.repo.url} target="_blank">{props.repo.name}</a></strong>
     <br /><strong>Owner:</strong> <span className="repoLabels">{props.repo.owner}</span>
-    <br /><strong>Date:</strong> <span className="repoLabels">{props.repo.created_at.slice(0, props.repo.created_at.indexOf('T'))}</span>
+    <br /><strong>Last Updated:</strong> <span className="repoLabels">{props.repo.updated_at.slice(0, props.repo.updated_at.indexOf('T'))}</span>
     </p>
   </div>
 )
